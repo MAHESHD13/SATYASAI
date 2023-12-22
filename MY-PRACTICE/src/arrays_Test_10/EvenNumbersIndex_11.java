e23e8d329ad2f7bdad68bf6d792c5ce33a6a2589
donga <donga@MSSD>
2023-12-22 21:26:43 +0530

    ArraysTest

---
+++ MY-PRACTICE/src/arrays_Test_10/EvenNumbersIndex_11.java
@@ -0,0 +1,20 @@
+package arrays_Test_10;
+
+public class EvenNumbersIndex_11 
+{
+	public static void main(String[] args) 
+	{
+		int[] numbers= {12,17,16,18,25};
+		int index=0;
+		for(int i=0;i<numbers.length;i++)
+		{
+			if(numbers[i]%2==0)
+			{
+				index=i;
+				System.out.println(index);
+			}
+		}
+		
+	}
+
+}
