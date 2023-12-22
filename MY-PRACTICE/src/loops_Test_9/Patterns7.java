9a634deb183c425f7e1b44bbe2f4e40be2af8376
donga <donga@MSSD>
2023-12-22 21:31:03 +0530

    LOOPS

---
+++ MY-PRACTICE/src/loops_Test_9/Patterns7.java
@@ -0,0 +1,16 @@
+package loops_Test_9;
+
+public class Patterns7
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ MY-PRACTICE/src/loops_Test_9/Patterns9.java
@@ -0,0 +1,16 @@
+package loops_Test_9;
+
+public class Patterns9
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j<=5;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
